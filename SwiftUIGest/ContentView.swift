1ed534aa046cf5e027f7eb230f0de1bf5ec47385
Ali Aghamirbabaei <Me@AliAghamirbabaei.com>
2022-05-13 22:44:50 +0430

    Initial Commit

---
+++ SwiftUIGest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGest
+//
+//  Created by Ali Aghamirbabaei on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGest/SwiftUIGestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGestApp.swift
+//  SwiftUIGest
+//
+//  Created by Ali Aghamirbabaei on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
